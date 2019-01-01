468a05d95d8e566e01eaadca79fcee4ccf021a00
wintrmut3 <36431739+wintrmut3@users.noreply.github.com>
2019-01-01 00:15:21 -0500

    autoscroller + content

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-
 // pronoun switching
 var gender = 0;
 var gp = "";
 var go = "";
+var active = false; //they answered the starting questions
 
 $("#b_male").click(function() {
   gender = 0;
@@ -25,26 +25,34 @@ switch (gender) {
     break;
 }
 console.log(active);
-function capitalize(word){
+
+function capitalize(word) {
   return word.charAt(0).toUpperCase() + word.slice(1);
 }
 var numMap = ["One", "Two", "Three", "Four", "Five", "Six", "Seven"];
-var randomTime = Math.round(Math.random()*3+3);
+var randomTime = Math.round(Math.random() * 3 + 3);
 
 
-const text = [" ",
+const text = [
+  " ",
   "<strong><br>Prologue</strong>",
   "Some things are better not understood...",
   "Like the crackle of a bright fire,",
   "the pale glimmer of moonlight reflected off an icy pond,",
   "or the pervasive quiet that follows a heavy snowfall.",
-  "<strong><br>PART I - Beginnings<br></strong>",
-  "\""+numMap[randomTime]+ " minutes!\" " + capitalize(gp)+ " says, " + go + " voice muffled by the thick scarf " + gp + " wears around " + go + " face.",
+  "<strong><br>ACT I - Beginnings<br></strong>",
+  "\"" + numMap[randomTime] + " minutes!\" " + capitalize(gp) + " says, " + go + " voice muffled by the thick scarf " + gp + " wears around " + go + " face.",
   "It is December 12th, " + new Date().getFullYear() + ". Tonight, the Geminid meteor shower reaches its peak.",
   "The two of you meander towards a high lookout, a long way up a winding path lit dimly by ancient gaslamps and silver moonlight.",
   "The forest envelops you, as the dark shadows of trees fill your field of vision.",
-  "The path is also lit by a multitude of fairy lights, new LEDs that someone has recently installed to illuminate the way to the lookout."
+  "The path is also lit by a multitude of fairy lights, new LEDs that someone has recently installed to illuminate the way to the lookout.",
+  "The walk is a pleasant one during daylight, but at night, especially on this night, the world seems to become alive with glinting snow and crystalline ice.",
+  "The steps are ancient, fahsioned of old wooden planks held fast by rusting nails.",
+  "Despite their isolation, someone has cleared them of ice and salted them.",
+  "<strong><br>ACT II - Ascent<br></strong>",
+  "It's not far to the lookout. You see a clear path ahead.",
 ];
+
 let t = 0;
 
 const timeline = [
@@ -61,6 +69,12 @@ const timeline = [
   flv,
   flv, //12 starting flavour text, then first choice
   chc,
+  cnq,
+  flv,
+  flv,
+  flv,
+  flv,
+  flv,
 ]
 const choiceFlags = [
   0,
@@ -78,6 +92,14 @@ const params = [
   text[8],
   text[9],
   text[10],
+  text[11],
+  ["Something makes you stop, and wait a moment.", "You look at the lights.", "You look to your left.", 0],
+  ["They are of many colours, a red like candycane or a blue like ice.<br>Someone has evidently taken a lot of care in arranging them.", capitalize(gp) + " is grinning at you, or at least you think so. It's hard to tell beneath the heavy scarf. " + capitalize(go) + " eyes sparkle, dancing with reflected starlight.", 0],
+  text[12],
+  text[13],
+  text[14],
+  text[15],
+  text[16],
 ]
 
 var audio = new Audio('crickets.wav');
@@ -86,7 +108,6 @@ audio.volume = 0.1;
 audio.play();
 
 
-var active = false; //they answered the starting questions
 
 const flvText = document.getElementById('flvtxt');
 const flvTextDiv = document.getElementById('flvTextBox');
@@ -103,17 +124,77 @@ function flv(text) {
   console.log(identifier);
   $("#" + identifier).hide();
   $("#" + identifier).fadeIn();
-  playStep(t);
+  //playStep(t);
 }
 
-function chc(description, choice1, choice2){
+
+
+function chc(array) {
+  let description = array[0];
+  let choice1 = array[1];
+  let choice2 = array[2];
+  let choiceNum = array[3];
   var identifier = 'choice_' + t;
-  textEl.setAttribute('id', identifier)
   const descriptor = document.createElement('p');
+  descriptor.setAttribute('id', identifier);
+  descriptor.setAttribute('style', "color: cyan");
   descriptor.innerHTML = description;
   flvTextDiv.appendChild(descriptor);
+
+  const b_choice1 = document.createElement('input');
+  var identifier_b1 = "choiceb_1" + t;
+  b_choice1.setAttribute('id', identifier_b1);
+  b_choice1.setAttribute('value', choice1);
+  b_choice1.setAttribute('class', 'button');
+  flvTextDiv.appendChild(b_choice1);
+
+  const b_choice2 = document.createElement('input');
+  var identifier_b2 = "choiceb_2" + t;
+  b_choice2.setAttribute('id', identifier_b2);
+  b_choice2.setAttribute('value', choice2);
+  b_choice2.setAttribute('class', 'button');
+  flvTextDiv.appendChild(b_choice2);
+
+  $("#" + identifier_b1).hide();
+  $("#" + identifier_b1).fadeIn();
+  $("#" + identifier_b2).hide();
+  $("#" + identifier_b2).fadeIn();
   $("#" + identifier).hide();
   $("#" + identifier).fadeIn();
+
+  $("#" + identifier_b1).click(function() {
+    $("#" + identifier_b1).fadeOut(500);
+    $("#" + identifier_b2).fadeOut(500);
+    choiceFlags[choiceNum] = 1;
+  })
+  $("#" + identifier_b2).click(function() {
+    $("#" + identifier_b1).fadeOut(500);
+    $("#" + identifier_b2).fadeOut(500);
+    choiceFlags[choiceNum] = 2;
+  })
+}
+
+function cnq(array) {
+  let cnq1 = array[0];
+  let cnq2 = array[1];
+  let flag_idx = array[2];
+  var cnqText = document.createElement('p');
+  if (flag_idx == 1){
+    cnqText.innerHTML = cnq1;
+  }
+  else{
+    cnqText.innerHTML = cnq2;
+  }
+  // switch (flag_idx){
+  //   case 1:
+  //     cnqText.innerHTML = cnq1;
+  //   break;
+  //   case 2:
+  //     cnqText.innerHTML = cnq2;
+  //   break;
+  // }
+  flvTextDiv.appendChild(cnqText);
+  console.log(cnq2);
 }
 
 //const track = [flv(text[0]), flv(text[1]), flv(text[2])];
@@ -125,7 +206,17 @@ function playStep(timestep) {
   stepSound.play();
 }
 
-
+// function scrollingAuto(){
+//   window.scrollBy(0,100);
+//   varx= setTimeout('scrollingAuto', 10);
+//   console.log("scrolling!");
+// }
+// scrollingAuto();
+function autoScroll(){
+  setInterval(function(){ console.log("SCROLLING..."); window.scrollBy(0,1) }, 10); 
+  //setInterval(function(){ window.scrollBy(10); console.log("SCROLLING..."); }, 10);
+}
+autoScroll()
 function update() {
   timeline[t](params[t]);
 }
@@ -136,7 +227,7 @@ $(document).ready(function() {
 
 
 $(document).click(function() {
-  if(active){
+  if (active) {
     console.log("Clicked!" + "Time = " + t);
     t++;
     update();
